0c56e21f973efdde348de830aca961c3260890a2
aaa <aaa>
2023-01-04 23:33:41 -0300

    aa

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log('Hello World!');
+const RandomImg = () => {
+  const images = [
+  'nada',
+  './template.jpg',
+  './cat.jpg',
+  './dahyun.jpg',
+  './eu.jpg'
+];
+
+  const random = Math.floor(Math.random() * images.length) + 1;
+  if (random == 5) {
+    RandomImg();
+  } else {
+    const profileImage = images[random];
+
+    document.getElementById("imgId").src = profileImage;
+  }
+}
+
+window.onload = RandomImg();
